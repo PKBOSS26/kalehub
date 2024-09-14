1e5e1b9fd21c92c346d632885ce21818078eb6ca
Pratik Kale <147796613+PKBOSS26@users.noreply.github.com>
2024-09-14 13:02:32 +0530

    project created

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,6 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen ">
-        {/* <Image
-          src="/path/to/your/logo.png" // Update with your logo path
-          alt="Logo"
-          width={150}
-          height={150}
-        /> */}
         <h1 className="text-2xl font-bold mt-4">Hello, Pratik Here!</h1>
         <p className="text-lg mt-2">Soon this site will be updated. Thanks for visiting!</p>
       </div>
